33e017035e985466abcb6d6a9f64a0288cf8a2d2
daikisuyama <suyama@craid-inc.com>
2022-04-25 14:31:58 +0900

    Clean confirm codes

--- code/default/functions.js
+++ code/default/functions.js
@@ -16,108 +16,59 @@ function sort_page_init(){
     });
 }
 
+// pathへparamsをPOSTでリクエスト
+function post(path,params) {
+    const form=document.createElement("form");
+    form.method="POST";
+    form.action=path;
+  
+    for(const key in params){
+      if(params.hasOwnProperty(key)){
+        const hiddenField=document.createElement("input");
+        hiddenField.type="hidden";
+        hiddenField.name=key;
+        hiddenField.value=params[key];
+        form.appendChild(hiddenField);
+      }
+    }
+    document.body.appendChild(form);
+    form.submit();
+}
+
 // Ajax：https://www.w3schools.com/xml/ajax_intro.asp
 // FormDataオブジェクト：https://developer.mozilla.org/ja/docs/Learn/Forms/Sending_forms_through_JavaScript
 
-function create_form_init(){
+function form_init(handle_file_path,alert_num_succeed,alert_num_fail,form_id,check_dialog){
     window.addEventListener("load",()=>{
-        function create_confirm(){
+        function confirm(){
             const XHR = new XMLHttpRequest();
             // FormDataオブジェクトとform要素の紐付け
             const FD  = new FormData(form);
             // リクエストの設定
-            XHR.open("POST","confirm/create_confirm.php");
+            XHR.open("POST",handle_file_path);
             // データの送信
             XHR.send(FD);
     
             // レスポンス後の処理
             XHR.addEventListener('readystatechange',()=>{
                 if(XHR.readyState===4 && XHR.status===200) {
-                    post("index.php",{alert_stmt:1,alert_text:XHR.responseText});
+                    post("index.php",{alert_stmt:alert_num_succeed,alert_text:XHR.responseText});
                 }else if(XHR.readyState===4){
-                    post("index.php",{alert_stmt:11,alert_text:XHR.responseText});
+                    post("index.php",{alert_stmt:alert_num_fail,alert_text:XHR.responseText});
                 }
             });
         }
         // form要素の取得
-        const form=document.getElementById("create_form");
+        const form=document.getElementById(form_id);
         // formのsubmitイベントを上書き
         form.addEventListener("submit",(event)=>{
             event.preventDefault();
-            if(check_dialog()){
-                create_confirm();
-            }
+            if(check_dialog())confirm();
         });
     });
 }
 
-function edit_form_init(){
-    window.addEventListener("load",()=>{
-        function edit_confirm(){
-            const XHR = new XMLHttpRequest();
-            // FormDataオブジェクトとform要素の紐付け
-            const FD  = new FormData(form);
-            // リクエストの設定
-            XHR.open("POST","confirm/edit_confirm.php");
-            // データの送信
-            XHR.send(FD);
-    
-            // レスポンス後の処理
-            XHR.addEventListener('readystatechange',()=>{
-                if(XHR.readyState===4 && XHR.status===200) {
-                    post("index.php",{alert_stmt:2,alert_text:XHR.responseText});
-                }else if(XHR.readyState===4){
-                    post("index.php",{alert_stmt:12,alert_text:XHR.responseText});
-                }
-            });
-        }
-        // form要素の取得
-        const form=document.getElementById("edit_form");
-        // formのsubmitイベントを上書き
-        form.addEventListener("submit",(event)=>{
-            event.preventDefault();
-            if(check_dialog()){
-                edit_confirm();
-            }
-        });
-    });
-}
-
-function delete_form_init(){
-    window.addEventListener("load",()=>{
-        function delete_confirm(){
-            const XHR = new XMLHttpRequest();
-            // FormDataオブジェクトとform要素の紐付け
-            const FD  = new FormData(form);
-            // リクエストの設定
-            XHR.open("POST","confirm/delete_confirm.php");
-            // データの送信
-            XHR.send(FD);
-    
-            // レスポンス後の処理
-            XHR.addEventListener('readystatechange',()=>{
-                if(XHR.readyState===4 && XHR.status===200) {
-                    post("index.php",{alert_stmt:3,alert_text:XHR.responseText});
-                }else if(XHR.readyState===4){
-                    post("index.php",{alert_stmt:13,alert_text:XHR.responseText});
-                }
-            });
-        }
-        // form要素の取得
-        const form=document.getElementById("delete_form");
-        // formのsubmitイベントを上書き
-        form.addEventListener("submit",(event)=>{
-            event.preventDefault();
-            if(delete_dialog()){
-                delete_confirm();
-            }
-        });
-    });
-}
-
-// このダイアログダサい
-// フォームのバリデーションチェック用ダイアログ（タイトルのみ）
-function check_dialog(){
+function title_validate(){
     let title=document.getElementById("form_title").value;
     if(title.length>31){
         alert("タイトルが長すぎます\n");
@@ -130,27 +81,17 @@ function check_dialog(){
     }
 }
 
-// 削除確認用ダイアログ
-function delete_dialog(){
-    return confirm('本当に削除しますか？');
+function create_form_init(){
+    check_dialog=title_validate;
+    form_init("confirm/create.php",1,11,"create_form",check_dialog);
 }
 
-function post(path, params, method='post') {
-    const form = document.createElement('form');
-    form.method = method;
-    form.action = path;
-  
-    for (const key in params) {
-      if (params.hasOwnProperty(key)) {
-        const hiddenField = document.createElement('input');
-        hiddenField.type = 'hidden';
-        hiddenField.name = key;
-        hiddenField.value = params[key];
-  
-        form.appendChild(hiddenField);
-      }
-    }
-  
-    document.body.appendChild(form);
-    form.submit();
+function edit_form_init(){
+    check_dialog=title_validate;
+    form_init("confirm/edit.php",2,12,"edit_form",check_dialog);
+}
+
+function delete_form_init(){
+    check_dialog=()=>confirm('本当に削除しますか？');
+    form_init("confirm/delete.php",3,13,"delete_form",check_dialog);
 }
